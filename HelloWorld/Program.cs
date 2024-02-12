455c67726fc9e644e048ccbb4ae796ff9c408bc0
Teodor Victor <teodorv55@gmail.com>
2024-02-12 14:40:23 +0200

    no message

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
-Console.WriteLine("Test");
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string myname = "Denis";
+            string message = "My name is " + myname;
+            string capsMessage = message.ToUpper();
+            string lowerCaseMessage = message.ToLower();
+            string name = "Teodor";
+            //Console.WriteLine(name.ToUpper());
+            //Console.WriteLine(lowerCaseMessage);
+            //Console.Read();
+            Console.WriteLine("Enter Something");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("You have entered {0}", userInput);
+            Console.WriteLine("Enter a string: ");
+            int asciiVallue = Console.Read();
+            Console.WriteLine("Ascii {0}", asciiVallue);
+            Console.ReadKey();
+        }
+
+
+    }
+}
