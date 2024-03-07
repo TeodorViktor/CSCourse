cfa0e75f9b0e58ac82a13eee5edea2b92669d52b
Teodor Victor <teodorv55@gmail.com>
2024-03-08 01:24:59 +0200

    GettersAndSetters

---
+++ Getters&Setters/Getters&Setters/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getters_Setters
+{
+    internal class Car
+    {
+        private string _name;
+        private int _hp;
+        public Car() { }
+        public string Name 
+        { 
+            get { return _name; }
+            set { _name = value; }
+        }
+        public void Details() 
+        {
+            Console.WriteLine(_name);
+        }
+    }
+}
---
+++ Getters&Setters/Getters&Setters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getters_Setters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car audi = new Car();
+            audi.Name = "BMW";
+            audi.Details();
+            Console.ReadKey();
+        }
+    }
+}
